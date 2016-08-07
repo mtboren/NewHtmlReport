a585dec23204e44014ecbd870262059c8c17c1e5
MTBoren <technology@ddayinc.com>
2016-08-06 21:10:49 -0400

    added InputObject ValueFromPipeline support in New-HtmlReport function

--- NewHtmlReport/New-HtmlReport.psm1
+++ NewHtmlReport/New-HtmlReport.psm1
@@ -4,7 +4,7 @@
 	New-HtmlReport -InputObject (dir c:\temp -filter *txt) -PreContent "blahh0","blahh1" -CssUri "/someFolder/someFile.css" -Property Name,LastWriteTime -Title "another test of output"
 	Create HTML output for an array of objects, with given stylesheet, and using built-in HTML for creating sortable table
 	.Example
-	New-HtmlReport -Title "My VMHost Report" -PreContent "<H3>VMHosts Info</H3>" -PostContent "Generated $(Get-Date -Format 'yyyy-MMM-dd HH:mm:ss \G\M\Tz')" -TableCaption "highlighted rows are of VMHosts that are not in 'Connected' state" -InputObject (Get-VMHost | Select Name,ConnectionState,MemoryTotalGB,@{n="bDoRowHighlight"; e={if ($_.ConnectionState -ne "Connected") {$true}}}) -RoundNumber -NumDecimalPlace 1 | Out-File -Encoding ascii c:\temp\myVMHostReport.htm
+	Get-VMHost | Select-Object Name,ConnectionState,MemoryTotalGB,@{n="bDoRowHighlight"; e={$_.ConnectionState -ne "Connected"}} | New-HtmlReport -Title "My VMHost Report" -PreContent "<H3>VMHosts Info</H3>" -PostContent "Generated $(Get-Date -Format 'yyyy-MMM-dd HH:mm:ss \G\M\Tz')" -TableCaption "highlighted rows are of VMHosts that are not in 'Connected' state" -RoundNumber -NumDecimalPlace 1 | Out-File -Encoding ascii c:\temp\myVMHostReport.htm
 	Create an HTML report for some VMHosts, highlighting any row where the given VMhost is in a state other than "Connected", and write the HTML to a file
 	.Example
 	New-HtmlReport -Title "My Combined Report" -PostContent "<BR />",(New-PageBodyTableHtml -InputObject (Get-Cluster) -Property Name, EVCMode, VsanEnabled) -InputObject (Get-Datastore) -Property Name,FreeSpaceGB,CapacityGB -RoundNumber -PreContent "<H3>Combined report of datastores and clusters</H3>" | Out-File -Encoding ascii c:\temp\myCombinedReport.htm
@@ -13,16 +13,17 @@
 	String
 #>
 function New-HtmlReport {
+	[CmdletBinding()]
 	param(
-		## the Uniform Resource Identifier (URI) of the CSS to apply to the HTML; examples:  "http://myserver.com/mystyle.css" or "/incl/style.css"
+		## The Uniform Resource Identifier (URI) of the CSS to apply to the HTML; examples:  "http://myserver.com/mystyle.css" or "/incl/style.css"
 		[string]$CssUri,
 		## Homogenous input objects (all have the same properties defined) from which to make the HTML table in the new report
-		[PSObject[]]$InputObject,
+		[parameter(ValueFromPipeline=$true, Mandatory=$true)][PSObject[]]$InputObject,
 		## Text/HTML to add after the closing </TABLE> tag
 		[string[]]$PostContent,
 		## Text/HTML to add before the opening <TABLE> tag
 		[string[]]$PreContent,
-		## Theproperties of the input objects to use in the output; if none specified, all properties are output
+		## The properties of the input objects to use in the output; if not specified, all properties are output
 		[string[]]$Property,
 		## String to place in the <title> </title> tag in the <HEAD> </HEAD>
 		[string]$Title,
@@ -30,57 +31,56 @@ function New-HtmlReport {
 		[string]$TableCaption,
 		## Switch:  Round numbers to sum number of decimal places?
 		[parameter(ParameterSetName="RoundNumbers")][switch]$RoundNumber,
-		## Number of decimal places to which to round
+		## Number of decimal places to which to round. Default is zero.
 		[parameter(ParameterSetName="RoundNumbers")][int]$NumDecimalPlace = 0
 	) ## end param
 
+	begin {
+		## the ModuleManifest, via ScriptsToProcess, "imports" the default values from the given config file <thisModuleDir>\New-HtmlReport_configItems.ps1
+		<# values that were imported from the default values file:  $hshConfigItems_NewHtmlReport with keys:
+			CssUri
+			HeadHtmlValue_NoTitleTag
+			TitleHtml
+			SortableTableCssClass
+		#>
+		## the start of the TABLE HTML
+		$strTableStartHtml = "`n<TABLE CLASS='$($hshConfigItems_NewHtmlReport["SortableTableCssClass"])'>"
+		## add caption tag to table, if given param is not $null
+		if ($null -ne $TableCaption) {$strTableStartHtml += "`n<CAPTION CLASS='tblCaption'>$TableCaption</CAPTION>"}
+		## the finish of the TABLE HTML
+		$strTableFinishHtml = "</TABLE>"
 
-	## the ModuleManifest, via ScriptsToProcess, "imports" the default values from the given config file <thisModuleDir>\New-HtmlReport_configItems.ps1
-	<# values that were imported from the default values file:  $hshConfigItems_NewHtmlReport with keys:
-		CssUri
-		HeadHtmlValue_NoTitleTag
-		TitleHtml
-		SortableTableCssClass
-	#>
+		## make string of the one or more PreContent strings
+		$strPreContent = if ($PSBoundParameters.ContainsKey("PreContent")) {$PreContent -join "<BR />`n"}
+		## make string of the one or more PreContent strings
+		$strPostContent = if ($PSBoundParameters.ContainsKey("PostContent")) {$PostContent -join "<BR />`n"}
 
-	## the start of the TABLE HTML
-	$strTableStartHtml = "`n<TABLE CLASS='$($hshConfigItems_NewHtmlReport["SortableTableCssClass"])'>"
-	## add caption tag to table, if given param is not $null
-	if ($null -ne $TableCaption) {$strTableStartHtml += "`n<CAPTION CLASS='tblCaption'>$TableCaption</CAPTION>"}
-	## the finish of the TABLE HTML
-	$strTableFinishHtml = "</TABLE>"
+		## an array in which to keep the input object to eventually be used in the end{} scriptblock
+		$arrInputObj = @()
+	} ## end begin
 
+	process {$arrInputObj += $InputObject}
 
-	## make string of the one or more PreContent strings
-	$strPreContent = if ($PreContent) {foreach ($strPreContentItem in $PreContent) {$strPreContentItem}}
-	## make string of the one or more PreContent strings
-	$strPostContent = if ($PostContent) {foreach ($strPostContentItem in $PostContent) {$strPostContentItem}}
+	end {
+		$hshParamsForNewPageBodyTable = @{
+			InputObject = $arrInputObj
+			Property = $Property
+			TableStartHtml = $strTableStartHtml
+			TableFinishHtml = $strTableFinishHtml
+		} ## end hsh
+		if ($RoundNumber) {$hshParamsForNewPageBodyTable["RoundNumber"] = $true; $hshParamsForNewPageBodyTable["NumDecimalPlace"] = $NumDecimalPlace}
 
-	## the precontent item(s), the table HTML, then the postcontent item(s)
-	$strBodyHtml = @"
-$strPreContent
-$(if ($InputObject) {
-	$hshParamsForNewPageBodyTable = @{
-		InputObject = $InputObject
-		Property = $Property
-		TableStartHtml = $strTableStartHtml
-		TableFinishHtml = $strTableFinishHtml
-	} ## end hsh
-	if ($RoundNumber) {$hshParamsForNewPageBodyTable["RoundNumber"] = $true; $hshParamsForNewPageBodyTable["NumDecimalPlace"] = $NumDecimalPlace}
-	New-PageBodyTableHtml @hshParamsForNewPageBodyTable}
-)
-$strPostContent
-"@
+		## make new object that holds all of the items to be passed to ConvertTo-Html cmdlet (or references to the variables with said info):
+		$hshConfigForConverttoHtml = @{
+			CssUri = if ($CssUri) {$CssUri} else {$hshConfigItems_NewHtmlReport["CssUri"]}
+			## generate the Pre / Body table / Post content string for the body
+			Body = ($strPreContent, (New-PageBodyTableHtml @hshParamsForNewPageBodyTable), $strPostContent) -join "`n"
+			Head = "<TITLE>$(if ($Title) {$Title} else {$hshConfigItems_NewHtmlReport["TitleHtml"]})</TITLE>`n$($hshConfigItems_NewHtmlReport["HeadHtmlValue_NoTitleTag"])"
+		} ## end hsh
 
-	## make new object that holds all of the items to be passed to ConvertTo-Html cmdlet (or references to the variables with said info):
-	$hshConfigForConverttoHtml = @{
-		CssUri = if ($CssUri) {$CssUri} else {$hshConfigItems_NewHtmlReport["CssUri"]}
-		Body = $strBodyHtml
-		Head = "<TITLE>$(if ($Title) {$Title} else {$hshConfigItems_NewHtmlReport["TitleHtml"]})</TITLE>`n$($hshConfigItems_NewHtmlReport["HeadHtmlValue_NoTitleTag"])"
-	} ## end hsh
-
-	## do the actual ConvertTo-Html, using (splatting) the given hashtable for params
-	ConvertTo-Html @hshConfigForConverttoHtml
+		## do the actual ConvertTo-Html, using (splatting) the given hashtable for params
+		ConvertTo-Html @hshConfigForConverttoHtml
+	} ## end end
 }
 
 <#	.Description
@@ -136,12 +136,7 @@ function New-PageBodyTableHtml {
 </TBODY>
 "@
 
-	@"
-$TableStartHtml
-$strTableHeadHtml
-$strTableBodyHtml
-$TableFinishHtml
-"@
+	($TableStartHtml, $strTableHeadHtml, $strTableBodyHtml, $TableFinishHtml) -join "`n"
 } ## end function
 
 
