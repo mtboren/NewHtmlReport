7e667184ccd883469bd4d4639ef784c32875bd7f
MTBoren <technology@ddayinc.com>
2016-09-15 20:06:43 -0400

    wrapped this portion of adding pipeline support for New-HtmlReport

--- testing/variousTestSnippets.ps1
+++ testing/variousTestSnippets.ps1
@@ -3,9 +3,11 @@
 ## make a new, GUID-named dir so as to avoid any overwrites and name-collisions
 $oTmpDir = New-Item -ItemType Directory -Path "${env:temp}\$(([System.Guid]::NewGuid()).Guid)"
 $strTmpDirFilespec = $oTmpDir.FullName
-
-## make a test report using just the contents of a temp dir with just the given properties (properties will be in property-name alphabetical order, since no -Property value specified to New-HtmlReport)
+## numbers for the tests
 $intTestNum = 0
+
+## pipeline testing
+## make a test report using just the contents of a temp dir with just the given properties (properties will be in property-name alphabetical order, since no -Property value specified to New-HtmlReport)
 Get-ChildItem ${env:temp} | Select-Object Mode, LastWriteTime, Length, Name | New-HtmlReport -Title "test HTML report" | Out-File -Encoding utf8 -FilePath $strTmpDirFilespec\testOut${intTestNum}.htm
 
 ## make a test report using just the contents of a temp dir with just the given properties, but setting the particular property order by having specified the -Property param to New-HtmlReport
--- todo.md
+++ todo.md
@@ -2,7 +2,7 @@
 - add ability to manage module configuration via cmdlet in module, instead of requiring user to manually edit configuration file in module directory
   - by leveraging JSON-based configs file, and new `Get-HtmlReportConfiguration` and `Set-HtmlReportConfiguration` cmdlets?
   - remove the global $str* variables currently in use by module (put them into the `hshConfigItems` hashtable)
-- add pipeline support to accept objects from pipeline
+- add pipeline support to accept objects from pipeline in New-HtmlReport and maybe New-PageBodyTableHtml
 - update tests for parameter usage in functions (instead of `if ($param) {}..`, use `if ($PSBoundParameters.ContainsKey('MyParam')) {}..`)
 - add some sample HTML files (but, with doctored paths to resource files)
 - for `New-HtmlReport` function:  make function to create HTML that uses datasource from JSON
